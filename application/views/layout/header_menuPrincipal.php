8c8d3a58feacf8870d033eb1a38cc75cfc8a8bcf
ARIIS29 <ari.sahulopez@gmail.com>
2024-12-31 18:16:29 -0600

    0.0.3 bienvenida a dinografia

--- application/views/layout/header_menuPrincipal.php
+++ application/views/layout/header_menuPrincipal.php
@@ -4,11 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Menú Principal</title>
+    <title>¡Bienvenido a Dinografía!</title>
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> -->
     <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap-css/bootstrap.css') ?>">
     <link rel="stylesheet" href="<?php echo base_url('assets/css/mystyle_menuPrincipal.css') ?>">
-    <link rel="shortcut icon" href="<?php echo base_url('almacenamiento/img/dino-lapiz.png') ?>" type="image/x-icon">
+    <link rel="shortcut icon" href="<?php echo base_url('almacenamiento/img/dinografia/dinografia-dino-lapiz.png') ?>" type="image/x-icon">
 </head>
 
 
@@ -17,11 +17,11 @@
         <nav class="navbar navbar-color navbar-expand-lg navbar-light fixed-top">
             <div class="container-fluid">
                 <div class="col-lg-3 col-md-3">
-                    <a class="nav-link" href="<?php echo base_url('MenuPrincipal') ?>"><img src="<?php echo base_url('almacenamiento/img/dinografia-log2.svg') ?>" alt="" class="img-fluid"></a>
+                    <a class="nav-link" href="<?php echo base_url('MenuPrincipal') ?>"><img src="<?php echo base_url('almacenamiento/img/dinografia/dinografia-log2.svg') ?>" alt="" class="img-fluid"></a>
                 </div>
-                <div class="col-lg-6 col-md-6 justify-aling-center">
+                <!-- <div class="col-lg-6 col-md-6 justify-aling-center">
                     <img src="<?php echo base_url('almacenamiento/img/titulos/menu_exploracion.png') ?>" alt="" class="img-fluid" width="100%">
-                </div>
+                </div> -->
                 <div class="col-lg-3 col-md-3 justify-aling-center">
                     <a href="<?php echo site_url('login/cerrar_sesion') ?>" id="cerrarSesion" class="btn btn-danger float-end">Cerrar sesion</a>
                 </div>
--- application/views/menuPrincipal/index.php
+++ application/views/menuPrincipal/index.php
@@ -1,110 +1,50 @@
 <section class="d-flex align-items-center mt-13" id="menuPrincipal">
     <div class="container">
         <div class="row">
-            <div class="col-lg-4 col-md-4 d-none d-sm-block btn-transicion text-center">
-                <a class="btn-trigger" href="javascript:void(0)" onclick="showFloatingButtons('group1')">
-                    <img src="<?php echo base_url('almacenamiento/img/botones/comienza_aventura.png') ?>" alt="" class="img-fluid enlargable" width="55%">
-                </a>
+            <div class="col-lg-4 col-md-4 d-none d-sm-block dino-hablando text-center">
+                <p>Haz clic en Dino para que hable o se detenga.</p>
+                <img src="<?php echo base_url('almacenamiento/img/dinografia/dinografia-dino-lapiz.png') ?>" alt="" class="img-fluid enlargable" id="dino" width="55%">
             </div>
-            <div class="col-lg-4 col-md-4 d-none d-sm-block btn-transicion text-center">
-                <a class="btn-trigger" href="javascript:void(0)" onclick="showFloatingButtons('group2')">
-                    <img src="<?php echo base_url('almacenamiento/img/botones/zona_aprendizaje-btn.png') ?>" alt="" class="img-fluid enlargable" width="55%">
-                </a>
+            <div class="col-lg-8 col-md-8 d-none d-sm-block btn-transicion text-center">
+                "¡Hola, bienvenido a Dinografía!
+                ¡Soy tu amigo Dino!, y estoy aquí para acompañarte en increíbles aventuras.
+                ¡Comencemos juntos! Primero, da clic en el botón de Guía del Explorador, para aprender los secretos de una escritura fantástica.
+                Recuerda: una buena escritura comienza con una buena postura. ¡Vamos a divertirnos mientras aprendemos!"
             </div>
-            <div class="col-lg-4 col-md-4 d-none d-sm-block btn-transicion text-center">
-                <a class="btn-trigger" href="javascript:void(0)" onclick="showFloatingButtons('group3')">
-                    <img src="<?php echo base_url('almacenamiento/img/botones/mi_progreso-btn.png') ?>" alt="" class="img-fluid enlargable" width="55%">
-                </a>
-            </div>
+            <audio id="dinoAudio" src="<?php echo base_url('almacenamiento/audios/bienvenido-dinografia.mp3') ?>" preload="auto"></audio>
         </div>
     </div>
 </section>
 
-<!-- Grupos de botones flotantes -->
-<div class="floating-buttons" id="group1" style="display: none; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);">
-    <button class="btn-close" onclick="closeFloatingButtons('group1')"></button>
-    <div class="row">
-        <p>¡Ha llegado el momento de empezar, explorador! Haz clic en uno de los botones y comencemos la aventura.</p>
-        <div class="col-6 text-center btn-transicion">
-            <a href="<?php echo base_url('escritura') ?>">
-                <img src="<?php echo base_url('almacenamiento/img/botones/bitacora-explorador.png') ?>" alt="" class="img-fluid enlargable" width="55%">
-            </a>
-        </div>
-        <div class="col-6 text-center btn-transicion">
-            <a href="<?php echo base_url('exploremos') ?>">
-                <img src="<?php echo base_url('almacenamiento/img/botones/exploremos-btn.png') ?>" alt="" class="img-fluid enlargable" width="55%">
-            </a>
-        </div>
-    </div>
-</div>
-
-<div class="floating-buttons" id="group2" style="display: none; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);">
-    <button class="btn-close" onclick="closeFloatingButtons('group2')"></button>
-    <div class="row">
-        <p>¡Sumérgete en la aventura del aprendizaje y haz clic en un botón para comenzar!</p>
-        <div class="col-6 text-center btn-transicion">
-            <a href="<?php echo base_url('RutaTrazo') ?>">
-                <img src="<?php echo base_url('almacenamiento/img/btn-rutatrazo.png') ?>" alt="" class="img-fluid enlargable" width="55%">
-            </a>
-        </div>
-        <div class="col-6 text-center btn-transicion">
-            <a href="<?php echo base_url('AventurasTrazo') ?>">
-                <img src="<?php echo base_url('almacenamiento/img/botones/aventuras_trazo_btn.png') ?>" alt="" class="img-fluid enlargable" width="55%">
-            </a>
-        </div>
-        <div class="col-6 text-center btn-transicion">
-            <a href="<?php echo base_url('grafismo') ?>">
-                <img src="<?php echo base_url('almacenamiento/img/botones/grafismo-btn.png') ?>" alt="" class="img-fluid enlargable" width="55%">
-            </a>
-        </div>
-        <div class="col-6 text-center btn-transicion">
-            <a href="<?php echo base_url('trazos_arena') ?>">
-                <img src="<?php echo base_url('almacenamiento/img/botones/arena-btn.png') ?>" alt="" class="img-fluid enlargable" width="55%">
-            </a>
-        </div>
-    </div>
-</div>
-
-<div class="floating-buttons" id="group3" style="display: none; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);">
-    <button class="btn-close" onclick="closeFloatingButtons('group3')"></button>
-    <div class="row">
-        <p> ¡Mide tu progreso, explorador!</p>
-        <div class="col-4 text-center btn-transicion">
-            <a href="<?php echo base_url('menuGalerias') ?>">
-                <img src="<?php echo base_url('almacenamiento/img/botones/galerias-btn.png') ?>" alt="" class="img-fluid enlargable" width="55%">
-            </a>
-        </div>
-        <div class="col-4 text-center btn-transicion">
-            <a href="<?php echo base_url('evaluacion_lecciones') ?>">
-                <img src="<?php echo base_url('almacenamiento/img/botones/evaluacion_lecciones_btn.png') ?>" alt="" class="img-fluid enlargable" width="55%">
-            </a>
-        </div>
-        <div class="col-4 text-center btn-transicion">
-            <a href="<?php echo base_url('evaluacion_ejercicios') ?>">
-                <img src="<?php echo base_url('almacenamiento/img/botones/evaluacion_ejercicios_btn.png') ?>" alt="" class="img-fluid enlargable" width="55%">
-            </a>
-        </div>
-    </div>
-</div>
-
-
 <script>
-    function showFloatingButtons(groupId) {
-        // Oculta los botones principales
-        document.getElementById('menuPrincipal').style.display = 'none';
+    // Referencias a elementos
+    const dino = document.getElementById('dino');
+    const audio = document.getElementById('dinoAudio');
 
-        // Oculta todos los grupos de botones flotantes
-        const allGroups = document.querySelectorAll('.floating-buttons');
-        allGroups.forEach(group => group.style.display = 'none');
+    // Inicia animación y audio al cargar la página
+    window.addEventListener('DOMContentLoaded', function() {
+        dino.classList.add('hablando'); // Inicia la animación
+        audio.play().catch(error => {
+            console.log("Error al reproducir el audio automáticamente:", error);
+        });
+    });
 
-        // Muestra el grupo seleccionado
-        document.getElementById(groupId).style.display = 'block';
-    }
+    // Manejo del clic en Dino
+    dino.addEventListener('click', function() {
+        if (dino.classList.contains('hablando')) {
+            dino.classList.remove('hablando'); // Detiene la animación
+            audio.pause(); // Pausa el audio
+            audio.currentTime = 0; // Reinicia el audio
+        } else {
+            dino.classList.add('hablando'); // Inicia la animación
+            audio.play().catch(error => {
+                console.log("Error al reproducir el audio:", error);
+            });
+        }
+    });
 
-    function closeFloatingButtons(groupId) {
-        // Oculta el grupo de botones flotantes
-        document.getElementById(groupId).style.display = 'none';
-        // Muestra los botones principales nuevamente
-        document.getElementById('menuPrincipal').style.display = 'block';
-    }
+    // Detener animación cuando termine el audio
+    audio.addEventListener('ended', function() {
+        dino.classList.remove('hablando'); // Detiene la animación
+    });
 </script>
