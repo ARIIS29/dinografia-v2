6f4851a58126df237b11326364267b77ddc35313
ARIIS29 <ari.sahulopez@gmail.com>
2025-05-10 00:29:58 -0600

    0.0.53 mi avance

--- application/views/aventuras_del_trazo/bosque_bambu/mi_avance_b.php
+++ application/views/aventuras_del_trazo/bosque_bambu/mi_avance_b.php
@@ -1,34 +1,25 @@
-<!-- Sección que contiene el contenido principal -->
-<!-- <div class="modal fade" id="modalInstrucciones" tabindex="-1" aria-labelledby="modalInstruccionesLabel" aria-hidden="true">
-    <div class="modal-dialog texto-modal-bambu">
-        <div class="modal-content mod-color">
-            <div class="modal-header">
-                <h5 class="modal-title" id="videoModalLabel">
-                    <img src="<?php echo base_url('almacenamiento/img/bosque_bambu/dino-indicaciones.png') ?>" alt="Img-Dino-Indicaciones" class="img-fluid dino-hablando me-3" width="8%"><b>¡Hola Explorador!</b>
-                </h5>
-                <button type="button" class="btn-close btn-danger" data-bs-dismiss="modal" aria-label="Close" title="Cerrar ventana"></button>
-            </div>
-            <div class="modal-body">
-                <p>
-                    ¡Bienvenido al bosque de bambú!
-                    En esta aventura, exploraremos juntos la letra <b>b</b>.
-                    A lo largo del viaje, descubrirás sus secretos y completarás misiones divertidas que te ayudarán a mejorar tus trazos y escribirla fácilmente. <br>
-                    ¡Juntos, exploremos la letra <b>b</b> y aprenderemos a escribirla correctamente! Prepárate para practicar y completar todas las misiones que el bosque de bambú tiene para ti. <br>
-                    ¡Comencemos con la exploración!
-                </p>
-
-            </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Entendido</button>
-            </div>
+<section class="container mt-12">
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12">
+            <table class="table display nowrap table-striped table-bordered scroll-horizontal table-hover text-center" cellspacing="0" name="table" id="table">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Fecha</th>
+                        <th>Actividad</th>
+                        <th>Cronómetro</th>
+                        <th>Aciertos</th>
+                        <th>Incorrectas</th>
+                        <th>Estrellas</th>
+                        <th>Evaluación</th>
+                        <th>Observaciones</th>
+                    </tr>
+                </thead>
+            </table>
         </div>
     </div>
-</div> -->
-
-
-<section class="d-flex justify-content-center align-items-center mt-10" id="menuPrincipal">
-    
 </section>
+<script src="<?php echo base_url('assets/js/evaluacion_ejercicios/index.js') ?>"></script>
 
 <script>
     // Mostrar el modal automáticamente cuando se carga la página
