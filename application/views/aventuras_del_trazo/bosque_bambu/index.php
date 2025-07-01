608ce372c87e08d2bf897b167433eb6c903384f1
ARIIS29 <ari.sahulopez@gmail.com>
2025-07-01 16:21:14 -0600

    1.0.37 botón explora la letra b y d

--- application/views/aventuras_del_trazo/bosque_bambu/index.php
+++ application/views/aventuras_del_trazo/bosque_bambu/index.php
@@ -1,5 +1,14 @@
-<section class="d-flex justify-content-center align-items-center mt-10" id="menuPrincipal">
+<section class="d-flex justify-content-center align-items-center mt-8" id="menuPrincipal">
     <div class="container">
+
+        <div id="tutorialModal" style="display:none; position:fixed; top:0; left:0; width:100%; height:100%; background-color:rgba(221, 247, 216, 0.8); justify-content:center; align-items:center; z-index:1000;">
+            <div style="position:relative; background:#fff; padding:10px; border-radius:10px; max-width:90%; width:600px;">
+                <iframe id="youtubeIframe" width="100%" height="315" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen src="">
+                </iframe>
+                <button id="cerrarTutorial" type="button" class="btn btn-danger" style="position:absolute; top:10px; right:10px;">Cerrar</button>
+            </div>
+        </div>
+
         <div class="col-lg-6 col-md-6 justify-aling-center text-center titulo-con-luz d-block d-sm-none mt-5">
             <h1 class="titulo-h1-bambu-movil">BOSQUE DE BAMBÚ</h1>
         </div>
@@ -14,13 +23,19 @@
             <!-- Botón 1 -->
             <div class="col-lg-3 col-md-3 col-6 btn-transicion d-none d-sm-block">
                 <a href="<?php echo base_url('letras/bosque_bambu/trazando_aventuras_b') ?>">
-                    <img src="<?php echo base_url('almacenamiento/img/bosque_bambu/btn-la_ruta_del_trazo.png') ?>" alt="Botón bosque de bambú" class=" img-fluid animated-button">
+                    <img src="<?php echo base_url('almacenamiento/img/bosque_bambu/btn-la_ruta_del_trazo.png') ?>" alt="Botón bosque de bambú" class=" img-fluid animated-button" width="90%">
                 </a>
             </div>
             <!-- Botón 2 -->
             <div class="col-lg-3 col-md-3 col-6 btn-transicion d-none d-sm-block">
                 <a href="<?php echo base_url('letras/bosque_bambu//explora_y_descubre_b') ?>">
-                    <img src="<?php echo base_url('almacenamiento/img/bosque_bambu/btn-explorando_la_letrab.png') ?>" alt="" class="img-fluid animated-button2">
+                    <img src="<?php echo base_url('almacenamiento/img/bosque_bambu/btn-explorando_la_letrab.png') ?>" alt="" class="img-fluid animated-button2" width="90%">
+                </a>
+            </div>
+
+            <div class="col-lg-12 col-md-12 col-sm-4 col-6 btn-transicion text-center mt-5">
+                <a id="abrirTutorial">
+                    <img src="<?php echo base_url('almacenamiento/img/bosque_bambu/btn_explora_letra_b.png') ?>" alt="Botón mi_avance_lapiz" class=" img-fluid animated-button" width="20%">
                 </a>
             </div>
 
@@ -45,6 +60,25 @@
 </section>
 
 <script>
+    const videoURL = "https://www.youtube.com/embed/v6QPxN7_Ojs"; // reemplaza con tu ID real
+
+    document.getElementById('abrirTutorial').addEventListener('click', function(e) {
+        e.preventDefault();
+        const modal = document.getElementById('tutorialModal');
+        const iframe = document.getElementById('youtubeIframe');
+
+        iframe.src = videoURL + "?autoplay=1"; // inicia reproducción automáticamente
+        modal.style.display = 'flex';
+    });
+
+    document.getElementById('cerrarTutorial').addEventListener('click', function() {
+        const modal = document.getElementById('tutorialModal');
+        const iframe = document.getElementById('youtubeIframe');
+
+        modal.style.display = 'none';
+        iframe.src = ""; // Detiene el video al cerrar
+    });
+
     // Referencias a elementos
     const dino = document.getElementById('dino');
     const audio = document.getElementById('dinoAudio');
--- application/views/aventuras_del_trazo/desierto/index.php
+++ application/views/aventuras_del_trazo/desierto/index.php
@@ -1,50 +1,84 @@
-<section class="d-flex justify-content-center align-items-center mt-10" id="menuPrincipal">
+<section class="d-flex justify-content-center align-items-center mt-8" id="menuPrincipal">
     <div class="container">
-        <div class="col-lg-6 col-md-6 justify-aling-center text-center titulo-con-luz d-block d-sm-none mt-5">
-            <h1 class="titulo-h1-desierto-movil">DESIERTO</h1>
-        </div>
-        <div class="col-lg-12 col-md-12 d-flex align-items-center">
-            <!-- Imagen -->
-            <img src="<?php echo base_url('almacenamiento/img/desierto/dino-indicaciones.png') ?>" alt="Img-Dino-Indicaciones" class="img-fluid dino-hablando me-3 d-none d-sm-block" id="dino" width="7%">
-            <!-- Texto -->
-            <p class="texto_indicaciones_desierto mb-0">¡Comencemos con la exploración, haciendo clic en un botón!</p>
-            <audio id="dinoAudio" src="<?php echo base_url('almacenamiento/audios/audios_b/audio_desierto.mp3') ?>" preload="auto"></audio>
-        </div>
-        <div class="row justify-content-center text-center">
-            <!-- Botón 1 -->
-            <div class="col-lg-3 col-md-3 col-6 btn-transicion d-none d-sm-block">
-                <a href="<?php echo base_url('letras/desierto/trazando_aventuras_d') ?>">
-                    <img src="<?php echo base_url('almacenamiento/img/desierto/btn_trazando_aventuras.png') ?>" alt="Botón trazando aventuras d" class=" img-fluid animated-button">
-                </a>
-            </div>
-            <!-- Botón 2 -->
-            <div class="col-lg-3 col-md-3 col-6 btn-transicion d-none d-sm-block">
-                <a href="<?php echo base_url('letras/desierto//explora_y_descubre_d') ?>">
-                    <img src="<?php echo base_url('almacenamiento/img/desierto/btn_explora_descubre.png') ?>" alt="Botón explora y descubre d" class="img-fluid animated-button2">
-                </a>
+        <div class="row d-flex justify-content-evenly">
+            <div id="tutorialModal" style="display:none; position:fixed; top:0; left:0; width:100%; height:100%; background-color:rgba(221, 247, 216, 0.8); justify-content:center; align-items:center; z-index:1000;">
+                <div style="position:relative; background:#fff; padding:10px; border-radius:10px; max-width:90%; width:600px;">
+                    <iframe id="youtubeIframe" width="100%" height="315" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen src="">
+                    </iframe>
+                    <button id="cerrarTutorial" type="button" class="btn btn-danger" style="position:absolute; top:10px; right:10px;">Cerrar</button>
+                </div>
             </div>
 
-        </div>
-
-        <div class="row justify-content-center text-center d-block d-sm-none">
-            <!-- Botón 1 -->
-            <div class="col-lg-3 col-md-3 col-sm-2 btn-transicion">
-                <a href="<?php echo base_url('letras/desierto/trazando_aventuras_d') ?>">
-                    <img src="<?php echo base_url('almacenamiento/img/desierto/btn_trazando_aventuras.png') ?>" alt="Botón desierto" class=" img-fluid">
-                </a>
+            <div class="col-lg-6 col-md-6 justify-aling-center text-center titulo-con-luz d-block d-sm-none mt-5">
+                <h1 class="titulo-h1-desierto-movil">DESIERTO</h1>
             </div>
-            <!-- Botón 2 -->
-            <div class="col-lg-3 col-md-3 col-sm-2 btn-transicion">
-                <a href="<?php echo base_url('letras/desierto/explora_y_descubre_d') ?>">
-                    <img src="<?php echo base_url('almacenamiento/img/desierto/btn_explora_descubre.png') ?>" alt="" class="img-fluid">
-                </a>
+            <div class="col-lg-12 col-md-12 d-flex align-items-center">
+                <!-- Imagen -->
+                <img src="<?php echo base_url('almacenamiento/img/desierto/dino-indicaciones.png') ?>" alt="Img-Dino-Indicaciones" class="img-fluid dino-hablando me-3 d-none d-sm-block" id="dino" width="7%">
+                <!-- Texto -->
+                <p class="texto_indicaciones_desierto mb-0">¡Comencemos con la exploración, haciendo clic en un botón!</p>
+                <audio id="dinoAudio" src="<?php echo base_url('almacenamiento/audios/audios_b/audio_desierto.mp3') ?>" preload="auto"></audio>
+            </div>
+            <div class="row justify-content-center text-center">
+                <!-- Botón 1 -->
+                <div class="col-lg-3 col-md-3 col-6 btn-transicion d-none d-sm-block">
+                    <a href="<?php echo base_url('letras/desierto/trazando_aventuras_d') ?>">
+                        <img src="<?php echo base_url('almacenamiento/img/desierto/btn_trazando_aventuras.png') ?>" alt="Botón trazando aventuras d" class=" img-fluid animated-button" width="90%">
+                    </a>
+                </div>
+                <!-- Botón 2 -->
+                <div class="col-lg-3 col-md-3 col-6 btn-transicion d-none d-sm-block">
+                    <a href="<?php echo base_url('letras/desierto//explora_y_descubre_d') ?>">
+                        <img src="<?php echo base_url('almacenamiento/img/desierto/btn_explora_descubre.png') ?>" alt="Botón explora y descubre d" class="img-fluid animated-button2" width="90%">
+                    </a>
+                </div>
+
+                <div class="col-lg-12 col-md-12 col-sm-4 col-6 btn-transicion text-center mt-4">
+                    <a id="abrirTutorial">
+                        <img src="<?php echo base_url('almacenamiento/img/desierto/btn_explora_letra_d.png') ?>" alt="Botón mi_avance_lapiz" class=" img-fluid animated-button" width="20%">
+                    </a>
+                </div>
+
             </div>
 
+            <div class="row justify-content-center text-center d-block d-sm-none">
+                <!-- Botón 1 -->
+                <div class="col-lg-3 col-md-3 col-sm-2 btn-transicion">
+                    <a href="<?php echo base_url('letras/desierto/trazando_aventuras_d') ?>">
+                        <img src="<?php echo base_url('almacenamiento/img/desierto/btn_trazando_aventuras.png') ?>" alt="Botón desierto" class=" img-fluid">
+                    </a>
+                </div>
+                <!-- Botón 2 -->
+                <div class="col-lg-3 col-md-3 col-sm-2 btn-transicion">
+                    <a href="<?php echo base_url('letras/desierto/explora_y_descubre_d') ?>">
+                        <img src="<?php echo base_url('almacenamiento/img/desierto/btn_explora_descubre.png') ?>" alt="" class="img-fluid">
+                    </a>
+                </div>
+
+            </div>
         </div>
-    </div>
 </section>
 
 <script>
+    const videoURL = "https://www.youtube.com/embed/JX_AP9rO4q4"; // reemplaza con tu ID real
+
+    document.getElementById('abrirTutorial').addEventListener('click', function(e) {
+        e.preventDefault();
+        const modal = document.getElementById('tutorialModal');
+        const iframe = document.getElementById('youtubeIframe');
+
+        iframe.src = videoURL + "?autoplay=1"; // inicia reproducción automáticamente
+        modal.style.display = 'flex';
+    });
+
+    document.getElementById('cerrarTutorial').addEventListener('click', function() {
+        const modal = document.getElementById('tutorialModal');
+        const iframe = document.getElementById('youtubeIframe');
+
+        modal.style.display = 'none';
+        iframe.src = ""; // Detiene el video al cerrar
+    });
+
     // Referencias a elementos
     const dino = document.getElementById('dino');
     const audio = document.getElementById('dinoAudio');
